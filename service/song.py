80743b58cdce3706cc8cd9f01a240eeaaab47a63
avelin123 <44392866+avelin123@users.noreply.github.com>
2023-04-18 12:37:19 +0100

    Add files via upload

--- service/song.py
+++ service/song.py
@@ -1,5 +1,5 @@
 from utils import song as SongFunction
-from db.song import CheckOrInsertSong
+from db.song import CheckOrInsertSong,get_all_songs
 import utils.song
 import pygame 
 
@@ -8,7 +8,7 @@ pygame.mixer.init()
 # service layer is the one that usually interacts to the api/gui or CLI . 
 # we will fetch the information about the song once it has been selected from the music player. 
 # the path can be anyone. 
-
+path_name_metadata = {}
 
 class SongService:
     def __init__(self, path):
@@ -21,11 +21,40 @@ class SongService:
         self.path = path
         self.pauseSong = False 
         self.current_time = 0 
-        # self.initialize =      
+        # self.initialize =     
+        self.set_existing_song_metadata()
+         
         
 
-
     
+    def set_existing_song_metadata(self):
+        songs_list = get_all_songs()
+        for songs in songs_list:
+            path_name_metadata[songs.path] = songs 
+        
+
+    @staticmethod
+    def get_song_names_from_pathurls(paths): 
+        name_list = []
+        for path in paths : 
+            try :
+
+                name_list.append(SongService.get_song_names_from_pathurl(path))
+            except Exception as e : 
+                name_list.append("Name Not Found")
+        return name_list
+
+    @staticmethod
+    def get_song_names_from_pathurl(path):
+        name = "Name Not Found"
+        try :
+            name = path_name_metadata[path]["title"]
+            
+        except Exception as e : 
+            name =  SongFunction.metaData(path)["title"]
+
+        return name 
+
     def play(self): 
         # You may need to add more values to the constructor to run this. 
         # SongFunction.play(self.path)
@@ -33,7 +62,7 @@ class SongService:
         try :
 
             song_id = CheckOrInsertSong(self.metadata)
-            
+            path_name_metadata[self.path] = self.metadata
             return song_id 
         except Exception as e : 
             print(e)
