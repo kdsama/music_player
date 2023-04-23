3c9adb26a68833e7f37b40d2b87e71bf64dca056
kdsama <kdsama5593@gmail.com>
2023-04-23 13:51:25 +0100

    added abstract base classes

--- service/playlist.py
+++ service/playlist.py
@@ -1,10 +1,11 @@
 from service.song import SongService
 from db.song import get_last_played_song
+from service.playlist_abc import PlaylistServiceABC
 
 
 
 
-class PlaylistService:
+class PlaylistService(PlaylistServiceABC):
     def __init__(self, songs):
         self.current_song_index = 0 
         # songs =  list of absolute paths, not relative paths 
@@ -33,13 +34,6 @@ class PlaylistService:
             if self.songs[index] == path_url :
                 self.songServiceObject = SongService(self.songs[index])
                 song_id = self.songServiceObject.play()     
-
-    def play_song_by_pathurl(self,path_url):
-        for index in range(0,len(self.songs)) :
-            if self.songs[index] == path_url :
-                self.songServiceObject = SongService(self.songs[index])
-                song_id = self.songServiceObject.play()     
-
 
     def showCurrentPlaylist(self):
         # This is incorrect . Here We have to fetch the names of the songs and just dislpay them , alongside how much time 
@@ -110,50 +104,3 @@ class PlaylistService:
         if song is None : 
             return ""
         return song["path"]
-        
-
-# class MusicPlayer:
-#     def __init__(self, music_files):
-#         pygame.init()
-#         pygame.mixer.init()
-#         self.music_files = music_files
-#         self.current_song_index = 0
-#         self.paused = False
-#         self.fast_forwarding = False
-#         self.rewinding = False
-
-#     def play_music(self):
-#         # Load and play the current song
-#         pygame.mixer.music.load(self.music_files[self.current_song_index])
-#         pygame.mixer.music.play()
-
-#     def play_next_song(self):
-#         # Increment the current song index
-#         self.current_song_index += 1
-
-#         # Check if we have reached the end of the playlist
-#         if self.current_song_index >= len(self.music_files):
-#             self.current_song_index = 0
-
-#         # Stop the current song
-#         pygame.mixer.music.stop()
-
-#         # Load and play the next song
-#         pygame.mixer.music.load(self.music_files[self.current_song_index])
-#         pygame.mixer.music.play()
-
-#     # ... other methods ...
-
-#     def wait_for_music_to_end(self):
-#         # Wait for the rest of the music to play
-#         while pygame.mixer.music.get_busy():
-#             pygame.event.pump()
-#             if self.paused:
-#                 pygame.mixer.music.pause()
-#             elif self.fast_forwarding:
-#                 self.fast_forwarding = False
-#             elif self.rewinding:
-#                 self.rewinding = False
-#             else:
-#                 pygame.mixer.music.unpause()
-            # pygame.time.wait(10)
--- service/song.py
+++ service/song.py
@@ -10,7 +10,7 @@ pygame.mixer.init()
 # we will fetch the information about the song once it has been selected from the music player. 
 # the path can be anyone. 
 path_name_metadata = {}
-path_name_metadata = {}
+
 
 class SongService:
     def __init__(self, path):
@@ -138,53 +138,3 @@ class SongService:
     @staticmethod
     def decrease_and_return_new_volume(from_vol,diff):
         return SongFunction.decrease_and_return_new_volume(from_vol,diff)
-
-# class MusicPlayer:
-#     def __init__(self, music_files):
-#         pygame.init()
-#         pygame.mixer.init()
-#         self.music_files = music_files
-#         self.current_song_index = 0
-#         self.paused = False
-#         self.fast_forwarding = False
-#         self.rewinding = False
-
-#     def play_music(self):
-#         # Load and play the current song
-#         pygame.mixer.music.load(self.music_files[self.current_song_index])
-#         pygame.mixer.music.play()
-
-#     def play_next_song(self):
-#         # Increment the current song index
-#         self.current_song_index += 1
-
-#         # Check if we have reached the end of the playlist
-#         if self.current_song_index >= len(self.music_files):
-#             self.current_song_index = 0
-
-#         # Stop the current song
-#         pygame.mixer.music.stop()
-
-#         # Load and play the next song
-#         pygame.mixer.music.load(self.music_files[self.current_song_index])
-#         pygame.mixer.music.play()
-
-#     # ... other methods ...
-
-#     def wait_for_music_to_end(self):
-#         # Wait for the rest of the music to play
-#         while pygame.mixer.music.get_busy():
-#             pygame.event.pump()
-#             if self.paused:
-#                 pygame.mixer.music.pause()
-#             elif self.fast_forwarding:
-#                 self.fast_forwarding = False
-#             elif self.rewinding:
-#                 self.rewinding = False
-#             else:
-#                 pygame.mixer.music.unpause()
-#             pygame.time.wait(10)
-
-#     def __del__(self):
-#         # Quit Pygame
-#         pygame.quit()
